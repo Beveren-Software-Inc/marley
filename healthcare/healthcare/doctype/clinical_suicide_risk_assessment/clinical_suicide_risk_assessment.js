d2cf3d3e25b065835341e1815fb017fef0089adc
maniamartial <martialmania19@gmail.com>
2026-04-02 17:48:01 +0300

    feat: pnss

--- healthcare/healthcare/doctype/clinical_suicide_risk_assessment/clinical_suicide_risk_assessment.js
+++ healthcare/healthcare/doctype/clinical_suicide_risk_assessment/clinical_suicide_risk_assessment.js
@@ -6,3 +6,31 @@
 
 // 	},
 // });
+
+frappe.ui.form.on('Clinical Risk Assessment', {
+    validate(frm) {
+        let score = 0;
+
+        if (frm.doc.has_ideation) score++;
+        if (frm.doc.has_plan) score++;
+        if (frm.doc.has_history) score++;
+        if (frm.doc.has_stressors) score++;
+        if (!frm.doc.has_support) score++;
+        if (!frm.doc.has_coping) score++;
+
+        frm.set_value('risk_score', score);
+
+        if (score <= 1) {
+            frm.set_value('risk_level', 'Low');
+        } else if (score <= 3) {
+            frm.set_value('risk_level', 'Medium');
+        } else {
+            frm.set_value('risk_level', 'High');
+        }
+
+        // Emergency override
+        if (frm.doc.plan_immediacy === "Immediate" || frm.doc.plan_immediacy === "Next 24 hours") {
+            frm.set_value('risk_level', 'Emergency');
+        }
+    }
+});
