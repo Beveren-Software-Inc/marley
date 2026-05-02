4ca0962fa84b7df1399abbddefedb6565b819f06
maniamartial <martialmania19@gmail.com>
2026-05-03 00:53:34 +0300

    feat: Update the acounting dimesinion on an invoice if missing

--- healthcare/hooks.py
+++ healthcare/hooks.py
@@ -295,6 +295,7 @@ doc_events = {
 			"healthcare.healthcare.doctype.patient_history_settings.patient_history_settings.update_medical_record",
 			"healthcare.healthcare.doctype.digi_whatsapp_notification_setup.digi_whatsapp_notification_setup.handle_on_update_after_submit",
 		],
+		"validate":"healthcare.healthcare.custom_doctype.sales_invoice.validate"
 	},
 	"Sales Invoice": {
 		"on_submit": "healthcare.healthcare.utils.manage_invoice_submit_cancel",
