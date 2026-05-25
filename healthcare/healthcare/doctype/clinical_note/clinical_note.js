18c26caacbe00b6b194288d58c9834d16d603a33
maniamartial <martialmania19@gmail.com>
2026-05-25 11:16:07 +0300

    feat: Able to lock clinical note

--- healthcare/healthcare/doctype/clinical_note/clinical_note.js
+++ healthcare/healthcare/doctype/clinical_note/clinical_note.js
@@ -1,29 +1,82 @@
 // Copyright (c) 2023, healthcare and contributors
 // For license information, please see license.txt
 
+const DOCTOR_PROGRESS_NOTE_TYPE = "Doctor Progress Note";
+const NOTE_LOCK_ROLES = ["Administrator", "System Manager", "Healthcare Administrator"];
+
+function can_lock_clinical_note() {
+	return frappe.user.has_role(NOTE_LOCK_ROLES);
+}
+
+function is_doctor_progress_note(frm) {
+	return frm.doc.clinical_note_type === DOCTOR_PROGRESS_NOTE_TYPE;
+}
+
+function apply_note_locked_state(frm) {
+	frm.set_read_only();
+	frm.disable_save();
+	let intro = __("This Doctor Progress Note is locked. Editing is disabled.");
+	if (frm.doc.locked_by) {
+		intro += "<br>" + __("Locked by {0}", [frm.doc.locked_by]);
+	}
+	if (frm.doc.locked_on) {
+		intro += "<br>" + __("Locked on {0}", [frappe.datetime.str_to_user(frm.doc.locked_on)]);
+	}
+	frm.set_intro(intro, true);
+}
+
+function lock_clinical_note(frm) {
+	frappe.call({
+		method: "healthcare.healthcare.doctype.clinical_note.clinical_note.lock_clinical_note",
+		args: { name: frm.doc.name },
+		freeze: true,
+		callback(r) {
+			if (!r.exc) {
+				frappe.show_alert({ message: __("Note locked"), indicator: "green" });
+				frm.reload_doc();
+			}
+		},
+	});
+}
+
 frappe.ui.form.on("Clinical Note", {
 	refresh(frm) {
+		frm.set_intro("");
 
+		if (frm.doc.note_locked) {
+			apply_note_locked_state(frm);
+			return;
+		}
+
+		if (!frm.is_new() && is_doctor_progress_note(frm) && can_lock_clinical_note()) {
+			frm.add_custom_button(__("Lock Note"), () => {
+				frappe.confirm(
+					__(
+						"Lock this Doctor Progress Note? It cannot be edited or deleted afterwards."
+					),
+					() => lock_clinical_note(frm)
+				);
+			}, __("Actions"));
+		}
 	},
-    terms_and_conditions: function(frm) {
-        set_terms_and_conditions(frm)
-    }
+	terms_and_conditions(frm) {
+		set_terms_and_conditions(frm);
+	},
 });
 
-var set_terms_and_conditions = function(frm, terms_and_conditions=''){
-    console.log(frm.doc.terms_and_conditions)
-    if (frm.doc.terms_and_conditions) {
-      return frappe.call({
-        method: 'erpnext.setup.doctype.terms_and_conditions.terms_and_conditions.get_terms_and_conditions',
-        args: {
-          template_name: frm.doc.terms_and_conditions || terms_and_conditions,
-          doc: frm.doc
-        },
-        callback: function (r) {
-          frm.set_value('note', r.message)
-        }
-      });
-    } else {
-      frm.set_value('note', '')
-    }
-  }
+var set_terms_and_conditions = function (frm, terms_and_conditions = "") {
+	if (frm.doc.terms_and_conditions) {
+		return frappe.call({
+			method:
+				"erpnext.setup.doctype.terms_and_conditions.terms_and_conditions.get_terms_and_conditions",
+			args: {
+				template_name: frm.doc.terms_and_conditions || terms_and_conditions,
+				doc: frm.doc,
+			},
+			callback(r) {
+				frm.set_value("note", r.message);
+			},
+		});
+	}
+	frm.set_value("note", "");
+};
--- healthcare/healthcare/doctype/clinical_note/clinical_note.py
+++ healthcare/healthcare/doctype/clinical_note/clinical_note.py
@@ -1,9 +1,68 @@
 # Copyright (c) 2023, healthcare and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
+from frappe.utils import now_datetime
+
+DOCTOR_PROGRESS_NOTE_TYPE = "Doctor Progress Note"
+NOTE_LOCK_ROLES = frozenset({"Administrator", "System Manager", "Healthcare Administrator"})
+
+
+def user_can_lock_clinical_note(user=None):
+	user = user or frappe.session.user
+	if user == "Administrator":
+		return True
+	return bool(set(frappe.get_roles(user)) & NOTE_LOCK_ROLES)
+
+
+def is_doctor_progress_note(doc):
+	return (doc.get("clinical_note_type") or "") == DOCTOR_PROGRESS_NOTE_TYPE
 
 
 class ClinicalNote(Document):
-	pass
+	def validate(self):
+		if self.is_new():
+			return
+
+		if frappe.db.get_value(self.doctype, self.name, "note_locked"):
+			frappe.throw(
+				_("This clinical note is locked and cannot be edited."),
+				title=_("Note Locked"),
+			)
+
+	def on_trash(self):
+		if self.note_locked:
+			frappe.throw(
+				_("This clinical note is locked and cannot be deleted."),
+				title=_("Note Locked"),
+			)
+
+
+@frappe.whitelist()
+def lock_clinical_note(name):
+	"""Permanently lock a Doctor Progress Note so it can no longer be edited."""
+	if not user_can_lock_clinical_note():
+		frappe.throw(_("You are not permitted to lock clinical notes."), frappe.PermissionError)
+
+	doc = frappe.get_doc("Clinical Note", name)
+
+	if not is_doctor_progress_note(doc):
+		frappe.throw(_("Only Doctor Progress Notes can be locked."))
+
+	if doc.note_locked:
+		frappe.throw(_("This note is already locked."))
+
+	frappe.db.set_value(
+		"Clinical Note",
+		name,
+		{
+			"note_locked": 1,
+			"locked_by": frappe.session.user,
+			"locked_on": now_datetime(),
+		},
+		update_modified=False,
+	)
+
+	return {"note_locked": 1, "locked_by": frappe.session.user, "locked_on": now_datetime()}
